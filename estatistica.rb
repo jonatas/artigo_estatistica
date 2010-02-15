8f74a85629bd865a22764cba3e4ce90c7fbd891f
Jonatas <jonatasdp@gmail.com>
2010-02-15 01:17:00 -0200

    adicionado rol, frequencia absoluta e explicações sobre hash

---
+++ estatistica.rb
@@ -0,0 +1,12 @@
+dados_brutos = [23, 31, 31, 21, 31, 26, 31, 22, 22, 
+                24, 31, 21, 22, 20, 22, 31, 21, 20]
+rol = dados_brutos.sort
+p rol # [20, 20, 21, 21, 21, 22, 22, 22, 22, 23, ...]
+fi = {}
+rol.each do |idade|
+  if not fi[idade]
+    fi[idade] = 1
+  else
+    fi[idade] += 1
+  end
+end
