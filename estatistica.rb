92853e159b3ad83dfaeccc031e4b2bfad3f0c5fa
Jonatas <jonatasdp@gmail.com>
2010-03-10 21:34:21 -0300

    frequencia absoluta acumulada

--- estatistica.rb
+++ estatistica.rb
@@ -1,7 +1,7 @@
 dados_brutos = [23, 31, 31, 21, 31, 26, 31, 22, 22, 
                 24, 31, 21, 22, 20, 22, 31, 21, 20]
 rol = dados_brutos.sort
-p rol # [20, 20, 21, 21, 21, 22, 22, 22, 22, 23, ...]
+#p rol # [20, 20, 21, 21, 21, 22, 22, 22, 22, 23, ...]
 fi = {}
 rol.each do |idade|
   if not fi[idade]
@@ -9,4 +9,19 @@ rol.each do |idade|
   else
     fi[idade] += 1
   end
+end
+fr = {}
+n = dados_brutos.size
+fi.each do |idade, frequencia_absoluta|
+  fr[idade] = ((frequencia_absoluta.to_f / n) * 100)
+end
+fac = {}
+acumulado = nil
+fi.keys.sort.each do |idade|
+  if not acumulado
+    acumulado = fi[idade]
+  else
+    acumulado += fi[idade]
+  end
+  fac[idade] = acumulado
 end
